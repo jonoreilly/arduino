f90d151b451553b7a0e0390ff0e56c0cd6a9d147
jonoreilly <jonoreilly007@gmail.com>
2017-09-13 20:08:10 +0200

    irattiny sony tv power code

--- attiny/irattiny/irattiny.ino
+++ attiny/irattiny/irattiny.ino
@@ -1,20 +1,38 @@
-int ir = 1, spd = 14;
+int iron = 1, spd = 14, iroff = 5;
+bool sonypower [12] = {1,0,1,0,1,0,0,1,0,0,0,0} ;
+float wait = 0.5, preamble = 2.5, data [2] = {0.75,1.25};
 
-void iron ( float milsec ) {
-float ciclos = milsec / 0.02631 ; //in mili seconds
-for ( int i = 0 ; i < ciclos ; i++ ) {
-  //this cilce lasts 0.000 026 31 secs at 38kHz
-  digitalWrite(ir, HIGH);
-  delayMicroseconds(spd);
-  digitalWrite(ir, LOW);
-  delayMicroseconds(spd);  
+// ______------_--_-_--_-_--_-_-_--_-_-_-_-_________
+
+
+
+void ir ( int pin, float milsec ) {
+  float ciclos = milsec / 0.02631 ; //in mili seconds
+  for ( int i = 0 ; i < ciclos ; i++ ) {
+    //this cilce lasts 0.000 026 31 secs at 38kHz
+    digitalWrite(pin, HIGH);
+    delayMicroseconds(spd);
+    digitalWrite(pin, LOW);
+    delayMicroseconds(spd);  
 }}
 
 
-void setup() {
-  pinMode (ir, OUTPUT);
+void output ( bool transmission [] ) {
+  ir (iron, preamble);
+  for (int e = 0; e < sizeof sonypower; e++ ) {
+    ir (iroff, wait);
+    ir (iron, data[transmission[e]]);
+    }
 }
 
+
+void setup() {
+  pinMode (iron, OUTPUT);
+  pinMode (iroff, OUTPUT);
+}
+
+
 void loop() {
-  iron(2);
+  delay(20000);
+  output(sonypower);
 }
